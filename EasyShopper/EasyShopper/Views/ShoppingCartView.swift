7727f58d2167bb7ca684a433cbaf3f628f999e49
Iiro Alhonen <iiro.alhonen@gmail.com>
2020-06-29 16:23:26 +0300

    Basic SwiftUI setup

---
+++ EasyShopper/EasyShopper/Views/ShoppingCartView.swift
@@ -0,0 +1,21 @@
+//
+//  ShoppingCartView.swift
+//  EasyShopper
+//
+//  Created by Iiro Alhonen on 29.6.2020.
+//  Copyright © 2020 Ka-ching. All rights reserved.
+//
+
+import SwiftUI
+
+struct ShoppingCartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShoppingCartView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShoppingCartView()
+    }
+}
